4e5ae64ecad9c6955736f481bb78f579d8c62315
ron9817 <ronak19982014@gmail.com>
2020-03-18 02:57:17 +0530

    Seller ui updated

--- resources/views/Home/seller.blade.php
+++ resources/views/Home/seller.blade.php
@@ -8,15 +8,12 @@
     .nav .nav-item{
         border: none!important;
         outline:none!important;
-    }
-    .nav .nav-item.nav-link{
-        border: none!important;
-        outline:none!important;
-        border-bottom:10px solid #dfgd;
+        font-size:20px;
+        font-weight:500;
     }
     .nav .nav-item.nav-link.active{
-        border: none!important;
-        outline:none!important;
+        border-bottom:2px solid #dcb444!important;
+        font-weight:700;
     }
     .nav-tabs{
         border-bottom:none!important;
@@ -34,14 +31,14 @@
         <div class="col mt-5">
             <div class="nav nav-tabs nav-fill pointer" id="seller" role="tablist">
 
-                <a class="nav-item nav-link active" data-toggle="tab" role="tab" aria-selected="true" id="product" data-target="#product_data">Products</a>
+                <a class="nav-item nav-link active mx-3" data-toggle="tab" role="tab" aria-selected="true" id="product" data-target="#product_data">Products</a>
 
-                <a class="nav-item nav-link" data-toggle="tab" role="tab" aria-selected="true" id="order" data-target="#order_data">Orders</a>
+                <a class="nav-item nav-link mx-3" data-toggle="tab" role="tab" aria-selected="true" id="order" data-target="#order_data">Orders</a>
 
             </div>
-            <div class="tab-content">
-                <div class="tab-pane container active" id="product_data">
-                    Products
+            <div class="tab-content my-3">
+                <div class="tab-pane container active mx-0" id="product_data">
+                    @include('Seller.partials.products')
                 </div>
                 <div class="tab-pane container" id="order_data">
                     Order
---
+++ resources/views/Seller/partials/products.blade.php
@@ -0,0 +1,46 @@
+<div class="row">
+    <div class="col">
+        <button class="btn btn-primary">  <i class="fa fa-angle-down" aria-hidden="true"></i> Add new product</button>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <div class="row">
+            <div class="col-4 p-5 left_hand_side">
+                <div class="header">Login</div>
+                <div class="text my-3">Get access to your Orders, Wishlist and Recommendations</div>
+                <div class="image m-auto"></div>
+            </div>
+            <div class="col-7 p-5 right_hand_side bg-white">
+                <form id="login-form">
+                    <div class="form-group">
+                        <label for="email-number">Enter Email/Mobile Number</label>
+                        <input type="text" name="email-number" class="form-control" id="email-number" placeholder="Enter Email/Mobile Number">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Enter Password</label>
+                        <div>
+                            <input type="text" name="password" class="d-inline-block form-control" id="password" placeholder="Enter Password">
+                            <span class="color-blue">Forgot?</span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn text-center p-2 login-button" type="button" id="login-form-button">Login</button>
+                    </div>
+                    <div class="form-group">
+                        <div class="text-center">OR</div>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn text-center p-2 otp-button">Request OTP</button>
+                    </div>
+                    <div class="form-group mt-5">
+                        <div class="text-center color-blue">New to Flipkart? Create an account</div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-1"  data-dismiss="modal">
+                <div class="modal-close">X</div>
+            </div>
+        </div>
+    </div>
+</div>
