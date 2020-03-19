58564918794f04fad5d5c8009a654f51ae07795a
ron9817 <ronak19982014@gmail.com>
2020-03-19 12:07:58 +0530

    Created add product form

--- resources/views/Seller/partials/products.blade.php
+++ resources/views/Seller/partials/products.blade.php
@@ -1,40 +1,42 @@
 <div class="row">
     <div class="col">
-        <button class="btn btn-primary">  <i class="fa fa-angle-down" aria-hidden="true"></i> Add new product</button>
+        <button class="btn btn-primary" data-toggle="modal" data-target="#add_product">  <i class="fa fa-angle-down" aria-hidden="true"></i> Add new product</button>
     </div>
 </div>
-<div class="row">
+<div class="row mx-0 my-2 {{--modal--}}" id="add_product">
     <div class="col">
         <div class="row">
             <div class="col-4 p-5 left_hand_side">
-                <div class="header">Login</div>
-                <div class="text my-3">Get access to your Orders, Wishlist and Recommendations</div>
+                <div class="header">Add Product</div>
+                <div class="text my-3">Make your  product reach pan India</div>
                 <div class="image m-auto"></div>
             </div>
             <div class="col-7 p-5 right_hand_side bg-white">
-                <form id="login-form">
+                <form id="add_product-form">
                     <div class="form-group">
-                        <label for="email-number">Enter Email/Mobile Number</label>
-                        <input type="text" name="email-number" class="form-control" id="email-number" placeholder="Enter Email/Mobile Number">
+                        <label for="title">Enter Title</label>
+                        <input type="text" name="title" class="form-control" id="title" placeholder="Enter Title">
                     </div>
                     <div class="form-group">
-                        <label for="password">Enter Password</label>
-                        <div>
-                            <input type="text" name="password" class="d-inline-block form-control" id="password" placeholder="Enter Password">
-                            <span class="color-blue">Forgot?</span>
-                        </div>
+                        <label for="description">Enter Description</label>
+                        <textarea name="description" class="form-control" id="description" placeholder="Enter Description"></textarea>
+                    </div>
+                    <div class="form-group custom-file">
+                        <label for="image" class="custom-file-label">Upload Image Products</label>
+                        <input type="file" name="image" class="form-control custom-file-input" id="image">
+                    </div>
+                    <div class="form-group mt-2">
+                        <label for="category">Select Category</label>
+                        <select class="custom-select" id="category" name="category">
+                            <option value="1">One</option>
+                        </select>
                     </div>
                     <div class="form-group">
-                        <button class="btn text-center p-2 login-button" type="button" id="login-form-button">Login</button>
+                        <label for="stock">Enter Stock</label>
+                        <input type="number" name="stock" class="form-control" id="stock" placeholder="Enter Stock">
                     </div>
                     <div class="form-group">
-                        <div class="text-center">OR</div>
-                    </div>
-                    <div class="form-group">
-                        <button class="btn text-center p-2 otp-button">Request OTP</button>
-                    </div>
-                    <div class="form-group mt-5">
-                        <div class="text-center color-blue">New to Flipkart? Create an account</div>
+                        <button class="btn text-center p-2 login-button" type="button" id="add_product-form-button">Add Product</button>
                     </div>
                 </form>
             </div>
