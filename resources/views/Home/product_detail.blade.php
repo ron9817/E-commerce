8267af42ff2233ef1d425ece30fc38506f8495e5
ron9817 <ronak19982014@gmail.com>
2020-04-06 03:27:25 +0530

    working on detail product design

--- resources/views/Home/product_detail.blade.php
+++ resources/views/Home/product_detail.blade.php
@@ -12,34 +12,62 @@
 @endpush
 @push('css')
 <style>
+.container{
+    height:40rem!important;
+}
 .product-container-lhs{
-    width:25rem;
     /* background-color: #000; */
+    width:38%;
+    height:100%;
+    border: 1px solid #00000026;
+}
+.product-container-rhs{
+    /* background-color: #000; */
+    width: 58%;
+    height:100%;
     border: 1px solid #00000026;
 }
 .product-container-lhs .image{
     background-color: #d8d3d357;
-    width:100%;
-    height:30rem;
+}
+.product-container-lhs .cart{
+    background-color:#ff9f00;
+    border-radius:0px;
+    color:#fff;
+}
+.product-container-lhs .buy{
+    background-color:#fb641b;
+    border-radius:0px;
+    color:#fff;
 }
 </style>
 @endpush
 
 @section('content')
-<div class="container">
-    <div class="d-inline-block product-container-lhs m-1 p-2" data-id="{{$product['id']}}">
+<div class="container my-2">
+<div class="d-inline-block product-container-lhs">
+    <div>
+        <div>l;l</div>
+    </div>  
+</div>
+<div class="d-inline-block product-container-rhs">
+    <div>
+    
+    </div>
+</div>
+    <!-- <div class="d-inline-block product-container" data-id="{{$product['id']}}">
         <div class="image"></div>
-        <div class="my-2">
-            <button class="btn btn-primary">Add to cart</button>
-            <button class="btn btn-primary">Add to cart</button>
+        <div class="mt-3 d-flex justify-content-between">
+            <button class="btn w-100 mr-2 p-3 cart text-uppercase"><i class="fa fa-cart-plus text-white" aria-hidden="true"></i> add to cart</button>
+            <button class="btn w-100 p-3 buy text-uppercase"><i class="fa fa-bolt text-white" aria-hidden="true"></i> buy now</button>
         </div>
     </div>
-    <div class="d-inline-block p-3 m-4" data-id="{{$product['id']}}">
+    <div class="d-inline-block product-container" data-id="{{$product['id']}}">
         <div class="title">{{$product['title']}}</div>
         <div class="category my-1"><span class="font-weight-bold">Category:</span> <span>{{$product['category']}}</span></div>
         <div class="cost my-1"><span class="font-weight-bold">Cost:</span> <span>₹{{$product['price']}}/-</span></div>
         <div class="quantity my-1"><span class="font-weight-bold">Quantity:</span> <span>{{$product['stock']}}</span></div>
-    </div>
+    </div> -->
 </div>
 @endsection
 
