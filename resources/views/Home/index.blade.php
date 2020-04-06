1ccee471a9e7537891414e090399cd3e25c31290
ron9817 <ronak19982014@gmail.com>
2020-04-06 23:43:05 +0530

    Started working on footer of page

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -3,6 +3,23 @@
 @push('script')
 
 @endpush
+@push('css')
+<style>
+    .footer{
+        background-color:#172337;
+        border:1px solid #454d5e;
+    }
+    .footer div{
+        color:#fff;
+        display: inline-block;
+        width:16%;
+    }
+    .footer div div{
+        color:#fff;
+        display: block;
+    }
+</style>
+@endpush
 @section('body')
     <div class="row m-0">
         <div class="col p-0">
@@ -19,6 +36,12 @@
     <div class="row m-0">
         <div class="col p-0">
             @yield('content')
+        </div>
+    </div>
+
+    <div class="row m-0">
+        <div class="col p-0">
+            @include('Home.partials.footer')
         </div>
     </div>
 
---
+++ resources/views/Home/partials/footer.blade.php
@@ -0,0 +1,21 @@
+<div class="footer">
+<div>
+    <div>About Us</div>
+    <div>Contact Us</div>
+    <div>Careers</div>
+</div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+</div>
+<div class="footer p-3">
+    <div>Sell on Flipkart</div>
+    <div>Advertise</div>
+    <div>Gift Cards</div>
+    <div>Help Center</div>
+    <div>2020 copyright.</div>
+    <div>cards</div>
+</div>
