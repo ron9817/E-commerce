acf8823d16324783d7c8abf5a4d52ac076b06521
Ronak Suresh Karani <32196731+ron9817@users.noreply.github.com>
2020-05-03 16:55:48 +0530

    added my-profile route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/product/{id}', "HomeController@getProductDetails");
 Route::get('/cart', "CartController@getCart");
 Route::post('/cart', "CartController@postCart");
 Route::get('/check-out', "CartController@getCheckOut");
+Route::get('/my-profile',function(){ return "my profile";});
 Route::get('/seller', "SellerController@index");
 Route::get('/seller/dashboard', "SellerController@dashboard");
 Route::post('/seller/add-product', "SellerController@addProduct");
